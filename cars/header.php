222a1854ec22ac8e0512f20ffc012e466e6619ab
samuel <damosisamuel@gmail.com>
2023-02-03 11:12:11 +0100

    header & service

--- cars/header.php
+++ cars/header.php
@@ -33,7 +33,7 @@
                     <ul class="d-flex  list-unstyled myres pt-4">
                         <li><a href="index.php">Home</a></li>
                         <li><a href="about.php">About</a></li>
-                        <li>Service</li>
+                        <li><a href="service.php">Service</a></li>
                         <li>Cars</li>
                         <li>Contact</li>
                     </ul>
---
+++ cars/service.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <?php include('header.php'); ?>
+
+    </header>
+    <main>
+        <section class="container">
+            <div class="page-head">
+                <div>
+                    <h3 class="abt">SERVICE</h3>
+                    <p>HOME / ABOUT</p>
+                    <li class="list-unstyled" style="color:white;"><a href="index.php">HOME</a> / SERVICE</li>
+
+
+                </div>
+
+            </div>
+        
+
+        </section>
+
+        <section class="sect2 mb-4">
+                <div class="row">
+                    <div class="text-center">
+                        <h1>02</h1>
+                        <h2>Our Services</h2>
+                    </div>
+
+                </div>
+
+            </section>
+            <section class="renta container-fluid d-flex">
+                <div class="row justify-content-around mt-4">
+                    <div class="col-3   boche">
+                        <div class="row">
+                            <div class="col-3 pt-3">
+                                <div class="boxed ">
+
+                                    <i class="fa-solid fa-taxi p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3 ">
+
+                                <h1 class="digit">01</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren pt-3">CAR RENTAL</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-3 boche bo" >
+                        
+                        <div class="row " >
+                            <div class="col-3">
+                                <div class="boxed" >
+
+                                    <i class="fa fa-2x fa-money-check-alt p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3 ">
+
+                                <h1 class="digit" style="color: #1c1e32;">02</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren pt-3">CAR FINANCING</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-3  boche">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="boxed">
+
+                                    <i class="fa-solid fa-car p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3">
+
+                                <h1 class="digit">03</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren">CAR INSPECTION</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                
+               
+
+            </section>
+            <section class="renta container-fluid d-flex">
+                <div class="row justify-content-around mt-2">
+                    <div class="col-3   boche">
+                        <div class="row">
+                            <div class="col-3 pt-3">
+                                <div class="boxed ">
+
+                                    <i class="fa-solid fa-cogs p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3 ">
+
+                                <h1 class="digit">04</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren pt-3">AUTO REPAIRING</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-3 boche">
+                        
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="boxed">
+
+                                    <i class="fa-solid fa-spray-can p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3 ">
+
+                                <h1 class="digit">05</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren pt-3">AUTO PAINTING</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+                    <div class="col-3  boche">
+                        <div class="row">
+                            <div class="col-3">
+                                <div class="boxed">
+
+                                    <i class="fa-solid fa-pump-soap p-4" style="font-size: 40px; "></i>
+
+
+                                </div>
+
+                            </div>
+                            <div class="offset-5 col-3">
+
+                                <h1 class="digit">06</h1>
+
+                            </div>
+
+                        </div>
+                        <div class="row">
+                            <h3 class="ren">AUTO CLEANING</h3>
+                            <p class="text-secondary">kasd dolor no lorem nonunny sit labore tempor at justo rebum rebum stet,justo elitr dolor amet sit sea sed</p>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                
+               
+
+            </section>
+
+            <!-- new roll to be added -->
+            <section class="container">
+            <div class="page-head">
+                <div>
+                    <h2>50% OFF</h2>
+                    <h3>SPECIAL OFFER FOR NEW MEMBERS</h3>
+                    <p>Only for Sunday from 1st Jan to 30th Jan 2045</p>
+
+                    <button class="btn-lg btn-primary">Register Now</button>
+                  
+
+
+                </div>
+
+            </div>
+        
+
+        </section>
+
+
+    </main>
+    <footer>
+        <?php include('footer.php'); ?>
+
+    </footer>
+    
+</body>
+</html>
