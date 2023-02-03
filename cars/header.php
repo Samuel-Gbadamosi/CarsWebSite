96ff0321451146060347f53d5fad59318d63a105
samuel <damosisamuel@gmail.com>
2023-02-03 11:05:50 +0100

    index & header

---
+++ cars/header.php
@@ -0,0 +1,104 @@
+    <!-- header top -->
+    <div class="header-t row p-3">
+            <div class="col-3">
+                <ul class="d-flex justify-content-start  list-unstyled">
+                    <li class="pr-2"> <i class="fa-solid fa-phone"></i> +012 345 6789</li>
+                    <li><i class="fa-solid fa-envelope"> </i> | mail@domain.com</li>
+                </ul>
+
+
+            </div>
+            <div class="offset-7 col-2">
+                <ul class="d-flex justify-content-around list-unstyled">
+                    <li><i class="fa-brands fa-facebook"></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                    <li><i class="fa-brands fa-twitter"></i></li>
+                    <li><i class="fa-brands fa-google"></i></li>
+                    <li><i class="fa-brands fa-youtube"></i></li>
+                </ul>
+
+
+            </div>
+
+        </div>
+        <!-- header stops here -->
+        <div class="header-bot">
+            <div class="row">
+                <div class=" col-6">
+                    <h3>ROYAL CARS</h3>
+
+
+                </div>
+                <div class="offset-1 col-3 pt-2">
+                    <ul class="d-flex  list-unstyled myres pt-4">
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li>Service</li>
+                        <li>Cars</li>
+                        <li>Contact</li>
+                    </ul>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+        <div class=" heddy">
+            <div class="row p-3 ">
+                <div class="col-6">
+                    <div class="row">
+                        <div class=" col-5 ">
+                            <select class="ops" name="" placeholder="Pick me up" id="">
+                                <option value="">Pick-Up Location</option>
+                                <option value="1">Pavia</option>
+                                <option value="2">Milan</option>
+                                <option value="3">Bergamo</option>
+                            </select>
+
+                        </div>
+                        <div class="col-5">
+                            <select class="ops" name="" id="">
+                                <option value="">Drop Location</option>
+                                <option value="1">Milan</option>
+                                <option value="2">Bergamo</option>
+                                <option value="3">Sicily</option>
+                            </select>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <div class="col-6">
+                    <div class="row">
+                        <div class="col-4 ">
+                            <input style="border: 1px solid grey;" type="date" class="ops" name="" id="">
+
+
+                        </div>
+                        <div class="col-4">
+                            <select class="ops" name="" id="">
+                                <option value="">Select your Car</option>
+                                <option value="1">Benz</option>
+                                <option value="2">Bmw</option>
+                                <option value="3">Porsche</option>
+                            </select>
+
+                        </div>
+                        <div class="col-4">
+                            <button class="sk">Search</button>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+
+
+            </div>
+
+
+        </div>
