33ea099b523251c3c9514ab6a9a7ba877c22415b
samuel <damosisamuel@gmail.com>
2023-02-03 11:04:23 +0100

    about and footer page

---
+++ cars/about.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <?php include('header.php'); ?>
+
+    </header>
+    <main>
+        <section class="container">
+            <div class="page-head">
+                <div>
+                    <h3 class="abt">ABOUT</h3>
+                    <p>HOME / ABOUT</p>
+                    <li class="list-unstyled" style="color:white;"><a href="index.php">Home</a> / About</li>
+
+
+                </div>
+
+            </div>
+            <section class="sect2">
+                <div class="row">
+                    <div class="text-center">
+                        <h1>01</h1>
+                        <h2>WELCOME TO <small>Royal Cars</small></h2>
+                    </div>
+
+                </div>
+                <div class="row offset-3 pt-3 col-6 text-center">
+                    <img src="cars.png" alt="collections_cars">
+
+                    <p>Justo et eos et ut takimata sed sadipscing dolore lorem, et elitr labore labore voluptua no rebum sed, stet voluptua amet sed elitr ea dolor dolores no clita. Dolores diam magna clita ea eos amet, amet rebum voluptua vero vero sed clita accusam takimata. Nonumy labore ipsum sea voluptua sea eos sit justo, no ipsum sanctus sanctus no et no ipsum amet, tempor labore est labore no. Eos diam eirmod lorem ut eirmod, ipsum diam sadipscing stet dolores elitr elitr eirmod dolore. Magna elitr accusam takimata labore, et at erat eirmod consetetur tempor eirmod invidunt est, ipsum nonumy at et.</p>
+
+                </div>
+
+            </section>
+            <section class="sect3 ml-5">
+                <div class="row mt-4">
+                    <div class="col-4">
+                        <div class="box1 text-center">
+                            <div class="box  col-4 list-unstyled">
+                                <li><i class="fa-solid fa-headphones" style="font-size: 40px; "></i></li>
+  
+                            </div>
+                            <div class="offset-3 col-8">
+                                <p class="myh">24/7 Car Rental Support</p>
+
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="col-4">
+                        <div class="box1 text-center" style="background-color: #2B2E4A; ;">
+                            <div class="box  col-4 list-unstyled">
+                                <li><i class="fa-solid fa-car" style="font-size: 40px; "></i></li>
+
+                                
+                            </div>
+                            <div class="offset-3 col-8">
+                                <p class="myh" style="color: white;">Car Reservation Anytime</p>
+
+
+                            </div>
+                          
+
+
+                        </div>
+
+                        
+
+                    </div>
+                    <div class="col-4">
+                        <div class="box1 text-center">
+                            <div class="box  col-4 list-unstyled">
+                                <li><i class="fa-solid fa-location-dot" style="font-size: 40px; "></i></li>
+                            
+                            </div>
+                            <div class="offset-3 col-8">
+                                <p class="myh">Lots of Pickup Locations</p>
+                            </div>
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </section>
+
+        </section>
+
+
+    </main>
+    <footer>
+        <?php include('footer.php'); ?>
+
+    </footer>
+    
+</body>
+</html>
---
+++ cars/footer.php
@@ -0,0 +1,80 @@
+<div class="row">
+            <div class="col-3">
+            <div class="pest">
+                <h2 class="myhr pl-3">GET IN TOUCH</h2>
+                
+                <ul class="list-unstyled p-2">
+                    <li><i class="fa fa-map-marker-alt p-2"></i> 123 Street , New York , USA</li>
+                    <li><i class="fa fa-phone p-2"></i> +012 345 67890</li>
+                    <li><i class="p-2 fa-regular fa-envelope"></i> mail@domain.com</li>
+
+                </ul>
+
+                <p class="fl ml-4">FOLLOW US</p>
+
+                <ul class="d-flex justify-content-around  list-unstyled boxr">
+                    <li><i class="fa-brands fa-twitter "></i></li>
+                    <li><i class="fa-brands fa-facebook"></i></li>
+                    <li><i class="fa-brands fa-linkedin "></i></li>
+                    <li><i class="fa-brands fa-instagram"></i></li>
+                </ul>
+
+            </div>
+
+            </div>
+            <div class="col-3 ">
+                <h2 class="myhr">USEFUL LINKS</h2>
+                <ul class="list-unstyled bty ">
+                    <li><i class="fa fa-angle-right"></i>Private Policy</li>
+                    <li><i class="fa fa-angle-right"></i>Term & Condition</li>
+                    <li><i class="fa fa-angle-right"></i>New Member Registeration</li>
+                    <li><i class="fa fa-angle-right"></i>Affiliate Programme</li>
+                    <li><i class="fa fa-angle-right"></i>Return & Refund</li>
+                    <li><i class="fa fa-angle-right"></i>Help & FAQS</li>
+                </ul>
+
+
+            </div>
+            <div class="col-3 ">
+                <h2 class="myhr">CAR GALLERY</h2>
+
+                <div class="row">
+                    <div class="col-4">
+                        <img class="w-100" src="ft1pic.jpeg" alt="">
+                        <img class="w-100 mt-2" src="ft4.jpeg" alt="">
+                        
+
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100" src="ft2.jpeg" alt="">
+                        <img class="w-100 mt-2" src="ft5.jpeg" alt="">
+
+
+                    </div>
+                    <div class="col-4">
+                        <img class="w-100" src="ft3.jpeg" alt="">
+                        <img class="w-100 mt-2" src="ft6.jpeg" alt="">
+
+
+
+                    </div>
+
+                </div>
+
+
+            </div>
+            <div class="col-3 ">
+            <h2 class="myhr">NEWSLETTER</h2>
+
+            <p class="vol">Volup amet magna clita tempor.Tempor sea eos vero ipsum.Lorem lorem sit sed elitr sed kasd et</p>
+
+            <div>
+                <input class="myemail" type="email" placeholder="Your Email" name="" id="">
+                <button class="btn-lg btn-" style="background-color: orange; border: none; color: white; font-size: 19px;"><small>SIGN UP</small> </button>
+
+                <p class="vol pt-4">Lorem sit sed elitr sed kasd et</p>
+            </div>
+
+            </div>
+
+        </div>
